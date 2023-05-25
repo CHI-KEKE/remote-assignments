0d1f42c7c2c828c9a77ced92435b12ac8911f462
CHI-KEKE <chilly37647@gmail.com>
2023-05-25 18:37:30 +0800

    Assignment-3

---
+++ Week-1/Assignment-3/Assignment-3.js
@@ -0,0 +1,38 @@
+function countAandB(input) {
+// your code here
+    let count = 0;
+    input.forEach((item) => {
+        if(item === "a" || item === "b"){
+            count++;
+        }
+    })
+    return count;
+    
+}
+function toNumber(input) {
+// your code here
+    const references ={
+        a:1,
+        b:2,
+        c:3,
+        d:4,
+        e:5,
+    };
+    let result = [];
+
+    input.forEach((item) => {
+        if(references.hasOwnProperty(item)){
+            result.push(references[item])
+        };
+        
+    })
+    return result;
+}
+let input1 = ['a', 'b', 'c', 'a', 'c', 'a', 'c'];
+
+
+console.log(countAandB(input1)); // should print 4 (3 ‘a’ lettersand 1 ‘b’ letter)
+console.log(toNumber(input1)); // should print [1, 2, 3, 1, 3, 1, 3]
+let input2 = ['e', 'd', 'c', 'd', 'e'];
+console.log(countAandB(input2)); // should print 0
+console.log(toNumber(input2)); // should print [5, 4, 3, 4, 5]
