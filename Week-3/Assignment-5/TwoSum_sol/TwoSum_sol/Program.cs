a957982084067f753235f812157b87e9257b43f3
Allen Lin <chilly37647@gmail.com>
2023-06-10 09:43:29 +0800

    Handling TwoSum Week-3 Assignment-5

---
+++ Week-3/Assignment-5/TwoSum_sol/TwoSum_sol/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace TwoSum_sol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            //Test 1 Valid input with one pair fullfilled
+            int[] nums1 = { 3, 9, 8, 2 };
+            int target1 = 10;
+
+            try
+            {
+                int[] result1 = solution.TwoSum(nums1, target1);
+                Console.WriteLine($"Testing! [{string.Join(",", nums1)}], Target : {target1}");
+                Console.WriteLine($"The result of Indices : [{result1[0]},{result1[1]}]");
+                Console.ReadLine();
+            }
+
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine($"Testing! [{string.Join(", ", nums1)}], Target : {target1}");
+                Console.WriteLine($"Exception : {ex.Message}");
+                Console.ReadLine();
+            }
+
+            // Test 2: Valid input with multiple pairs fullfilled
+            int[] nums2 = { 3, 2, 4, 5, 6, 7, 8 };
+            int target2 = 9;
+            int[] result2 = solution.TwoSum(nums2, target2);
+            Console.WriteLine($"Testing! [{string.Join(", ", nums2)}], Target: {target2}");
+            Console.WriteLine($"Indices: [{result2[0]}, {result2[1]}]"); 
+            Console.WriteLine();
+
+
+            // Test 3: Valid input with negative numbers
+            int[] nums3 = { -3, -8, -1, -6 };
+            int target3 = -9;
+            int[] result3 = solution.TwoSum(nums3, target3);
+            Console.WriteLine($"Testing! [{string.Join(", ", nums3)}], Target: {target3}");
+            Console.WriteLine($"Indices: [{result3[0]}, {result3[1]}]"); 
+        }
+    }
+}
---
+++ Week-3/Assignment-5/TwoSum_sol/TwoSum_sol/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum_sol
+{
+    internal class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            if (nums == null)
+            {
+                throw new ArgumentNullException("Null array of input.");
+            }
+
+            if (nums.Length < 2)
+            {
+                throw new ArgumentException("The input array must contain at least 2 elements.");
+            }
+
+            var numberIndices = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (numberIndices.TryGetValue(complement, out int complementIndex))
+                {
+                    return new int[] { complementIndex, i };
+                }
+                numberIndices[nums[i]] = i;
+            }
+
+            throw new ArgumentException("Cannot find a pair of numbers of TwoSum.");
+        }
+
+    }
+}
